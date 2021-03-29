98ed632e3370ecbfd1de4bdea6ac2df850d4078f
Max <maxdv31@gmail.com>
2021-03-29 19:40:57 +0200

    new stack in the app stack

--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -1,24 +1,49 @@
 import React, { useContext } from 'react';
-import {View, TouchableOpacity, Text} from 'react-native';
+import {View} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Antdesign from 'react-native-vector-icons/AntDesign';
 
 import {Home} from '../screens/Home/Home';
 import { AuthContext } from './AuthProvider';
+import { Profile } from '../screens/Profile/Profile';
+import { Settings } from '../screens/settings/Settings';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const FeedStack = ({navigation}) => {
+const HomeStack = () => {
+  return (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Home"
+      component={Home}
+      options={{
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          color: '#2e64e5',
+          fontFamily: 'Kufam-SemiBoldItalic',
+          fontSize: 18,
+        },
+        headerStyle: {
+          shadowColor: '#fff',
+          elevation: 0,
+        },
+      }}
+    />
+    
+  </Stack.Navigator>
+  );
+};
+
+const ProfileStack = () => {
   const {logout} = useContext(AuthContext);
   return (
   <Stack.Navigator>
     <Stack.Screen
-      name="RN Social"
-      component={Home}
+      name="Profile"
+      component={Profile}
       options={{
         headerTitleAlign: 'center',
         headerTitleStyle: {
@@ -32,8 +57,8 @@ const FeedStack = ({navigation}) => {
         },
         headerRight: () => (
           <View style={{marginRight: 10}}>
-            <FontAwesome5.Button
-              name="plus"
+            <Antdesign.Button
+              name="logout"
               size={22}
               backgroundColor="#fff"
               color="#2e64e5"
@@ -43,6 +68,31 @@ const FeedStack = ({navigation}) => {
         ),
       }}
     />
+    
+  </Stack.Navigator>
+  );
+};
+
+const SettingsStack = () => {
+  return (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Settings"
+      component={Settings}
+      options={{
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          color: '#2e64e5',
+          fontFamily: 'Kufam-SemiBoldItalic',
+          fontSize: 18,
+        },
+        headerStyle: {
+          shadowColor: '#fff',
+          elevation: 0,
+        },
+      }}
+    />
+    
   </Stack.Navigator>
   );
 };
@@ -61,18 +111,49 @@ const AppStack = () => {
 
   return (
     <Tab.Navigator
+      initialRouteName="Home"
       tabBarOptions={{
         activeTintColor: '#2e64e5',
       }}>
       <Tab.Screen
+        name="Profile"
+        component={ProfileStack}
+        options={({route}) => ({
+          tabBarLabel: 'Profile',
+          // tabBarVisible: route.state && route.state.index === 0,
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="account-settings-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        })}
+      />
+      <Tab.Screen
         name="Home"
-        component={FeedStack}
+        component={HomeStack}
         options={({route}) => ({
           tabBarLabel: 'Home',
           // tabBarVisible: route.state && route.state.index === 0,
           tabBarIcon: ({color, size}) => (
             <MaterialCommunityIcons
               name="home-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        })}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsStack}
+        options={({route}) => ({
+          tabBarLabel: 'Settings',
+          // tabBarVisible: route.state && route.state.index === 0,
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="account-settings-outline"
               color={color}
               size={size}
             />
